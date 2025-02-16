9a47481475238ef199c87e6808b46fe865decc2f
miyake-yuki <cle.neige@gmail.com>
2025-02-16 18:25:58 +0900

    complete e

--- rust-1.70.0/abc392/src/bin/e.rs
+++ rust-1.70.0/abc392/src/bin/e.rs
@@ -1,22 +1,36 @@
-use proconio::{marker::Usize1, *};
-
-type UnGraph = Vec<Vec<usize>>;
-
-fn ungraph_from_edges(nodes: usize, edges: &[(usize, usize)]) -> UnGraph {
-    let mut ungraph = vec![Vec::new(); nodes];
-    for (u, v) in edges {
-        ungraph[*u].push(*v);
-        ungraph[*v].push(*u);
-    }
-    ungraph
-}
+use petgraph::unionfind::UnionFind;
+use proconio::{fastout, input, marker::Usize1};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
     input! {
         n: usize, m: usize,
-        connections: [(Usize1, Usize1); m],
+        cables: [(Usize1, Usize1); m],
     }
 
-    let graph = ungraph_from_edges(n, &connections);
+    let mut uf: UnionFind<usize> = UnionFind::new(n);
+    let mut redundant_edges = Vec::new();
+    for (i, (u, v)) in cables.into_iter().enumerate() {
+        if uf.equiv(u, v) {
+            redundant_edges.push((i, u, v));
+        } else {
+            uf.union(u, v);
+        }
+    }
+
+    let mut roots = uf
+        .clone()
+        .into_labeling()
+        .into_iter()
+        .collect::<HashSet<_>>();
+
+    println!("{}", roots.len() - 1);
+    for &(i, u, v) in &redundant_edges {
+        if let Some(&root) = roots.iter().find(|&&root| !uf.equiv(root, u)) {
+            println!("{} {} {}", i + 1, v + 1, root + 1);
+            uf.union(root, u);
+            roots.remove(&root);
+        }
+    }
 }
